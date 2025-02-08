5f086de0e3dc79c5b79c5339ed264c10cc1f57f7
ori-edry <oriedry1991@gmail.com>
2025-02-08 17:23:01 +0200

    Update app.component.spec.ts

--- payoneer/src/app/app.component.spec.ts
+++ payoneer/src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'payoneer'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('payoneer');
+    expect(app.title).toEqual('Payoneer');
   });
 
   it('should render title', () => {
